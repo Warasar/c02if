1daaa2293ffddc0992b52879f3afd2918f00a6ec
Pavel <43359673+Warasar@users.noreply.github.com>
2020-05-19 16:55:03 +0900

    Add files via upload

---
+++ из2.5/2.5.cpp
@@ -0,0 +1,39 @@
+﻿/*Напишите программу, которая вводит вещественные координаты x1, y1,x2,y2,x3,y3 треx точек на плоскости.
+Если они являются вершинами равнобедренного треугольника, то программа выводит площадь треугольника,
+впротивномслучаевыводит −1*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x1,x2,x3,y1,y2,y3,a,b,c,s;
+    cout << "Vvedite 1 tochku: ";
+    cin >> x1 >> y1;
+    cout << "Vvedite 2 tochku: ";
+    cin >> x2 >> y2; 
+    cout << "Vvedite 3 tochku: ";
+    cin >> x3 >> y3;
+
+    a = sqrt(x1 * x1 + y1 * y1);
+    b = sqrt(x2 * x2 + y2 * y2);
+    c = sqrt(x3 * x3 + y3 * y3);
+
+    if (a == b)
+    {
+        s = (c * sqrt(a * a * 4 - c * c)) / 4;
+        cout << s;
+    }
+    else
+        if (a == c)
+        {
+            s = (b * sqrt(a * a * 4 - b * b)) / 4;
+            cout << s;
+        }
+        else
+            if (c == b)
+            {
+                s = (a * sqrt(c * c * 4 - a * a)) / 4;
+                cout << s;
+            }
+            else cout << "-1";
+    }
---
+++ из3.1/3.1.cpp
@@ -0,0 +1,54 @@
+﻿/*Напишите программу, которая по целому числу n, номеру месяца в году, выводит название этого месяца.
+Например, ввод: 1, вывод : "январь"; ввод: 2, вывод : "февраль".Для n > 12 или n < 1 программа должна печатать ERROR*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    setlocale(LC_ALL, "ru");
+    switch (n)
+    {
+    case 1:
+        cout << "ЯНВАРЬ";
+        break;
+    case 2:
+        cout << "ФЕВРАЛЬ";
+        break;
+    case 3:
+        cout << "МАРТ";
+        break;
+    case 4:
+        cout << "АПРЕЛЬ";
+        break;
+    case 5:
+        cout << "МАЙ";
+        break;
+    case 6:
+        cout << "ИЮНЬ";
+        break;
+    case 7:
+        cout << "ИЮЛЬ";
+        break;
+    case 8:
+        cout << "АВГУСТ";
+        break;
+    case 9:
+        cout << "СЕНТЯБРЬ";
+        break;
+    case 10:
+        cout << "ОКТЯБРЬ";
+        break;
+    case 11:
+        cout << "НОЯБРЬ";
+        break;
+    case 12:
+        cout << "ДЕКАБРЬ";
+        break;
+    default:
+        cout << "ERROR";
+        break;
+    }
+
+}
